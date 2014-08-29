fc6886e2a08b97f15864e75ea45f9d23ff2da5d1
Will Bryant <will.bryant@gmail.com>
2014-08-29 19:13:48 +1200

    ActionController::RecordIdentifier became ActionView::RecordIdentifier

--- lib/action_view/helpers/prototype_helper.rb
+++ lib/action_view/helpers/prototype_helper.rb
@@ -253,7 +253,7 @@ module ActionView
           #   page['blank_slate'].show             # => $('blank_slate').show();
           #   page['blank_slate'].show('first').up # => $('blank_slate').show('first').up();
           #
-          # You can also pass in a record, which will use ActionController::RecordIdentifier.dom_id to lookup
+          # You can also pass in a record, which will use ActionView::RecordIdentifier.dom_id to lookup
           # the correct id:
           #
           #   page[@post]     # => $('post_45')
@@ -263,7 +263,7 @@ module ActionView
               when String, Symbol, NilClass
                 JavaScriptElementProxy.new(self, id)
               else
-                JavaScriptElementProxy.new(self, ActionController::RecordIdentifier.dom_id(id))
+                JavaScriptElementProxy.new(self, ActionView::RecordIdentifier.dom_id(id))
             end
           end
 
